d1ee3a2f1b4ed571e92ad6995a7a57248976fcc4
Aidan Bush <aidandbush@gmail.com>
2019-11-15 14:37:36 -0700

    Implement adding elements to database, and relating functions

--- code/py_parse.py
+++ code/py_parse.py
@@ -1,10 +1,15 @@
 #!/bin/python3.8
 import ast
+import pg8000
+
+def trim_filename(filename):
+    return filename[:-3]
 
 class Visitor(ast.NodeVisitor):
-    def __init__(self, filename):
+    def __init__(self, filename, conn):
         self.filename = filename
-        self.import_name = filename[:-3]
+        self.conn = conn
+        self.import_name = trim_filename(filename)
         self.import_mappings = {}
         return
 
@@ -37,16 +42,30 @@ class Visitor(ast.NodeVisitor):
 
     def visit_FunctionDef(self, node):
         print("func", node.name, node.lineno, node.end_lineno)
-        # insert filename, func, start, end
+        
+        c = self.conn.cursor()
+        c.execute("INSERT INTO functions (filename, name, start_line, end_line) VALUES (%s, %s, %s, %s)",
+                (self.filename, node.name, node.lineno, node.end_lineno))
+        self.conn.commit()
+        c.close()
+
         self.generic_visit(node)
 
     def visit_AsyncFunctionDef(self, node):
         print("async func", node.name, node.lineno, node.end_lineno)
+        
+        c = self.conn.cursor()
+        c.execute("INSERT INTO functions (filename, name, start_line, end_line) VALUES (%s, %s, %s, %s)",
+                (self.filename, node.name, node.lineno, node.end_lineno))
+        self.conn.commit()
+        c.close()
+
         self.generic_visit(node)
 
     def visit_Call(self, node):
+        base = None
+
         if hasattr(node.func, "id"):
-            base = None
             func_name = node.func.id
         else:
             func_name = node.func.attr
@@ -55,10 +74,10 @@ class Visitor(ast.NodeVisitor):
                 a = a.value
             if type(a) == ast.Name:
                 base = a.id
-            else:
-                base = None
 
-        #print("call", "base", base, "func name", func_name)
+        if base == None or base == "self":
+            base = self.import_name
+
         if base in self.import_mappings:
             # add func call
             print("call", self.import_mappings[base], func_name)
@@ -67,24 +86,105 @@ class Visitor(ast.NodeVisitor):
 
         # add to db
 
+        c = self.conn.cursor()
+        c.execute("INSERT INTO func_call (filename, base_name, name, start_line, end_line) VALUES (%s, %s, %s, %s, %s)",
+                (self.filename, base, func_name, node.lineno, node.end_lineno))
+        self.conn.commit()
+        c.close()
+
         self.generic_visit(node)
 
     def visit_ClassDef(self, node):
         print("class", node.name, node.lineno, node.end_lineno)
+
+        # add to db
+
+        c = self.conn.cursor()
+        c.execute("INSERT INTO classes (filename, name, start_line, end_line) VALUES (%s, %s, %s, %s)",
+                (self.filename, node.name, node.lineno, node.end_lineno))
+        self.conn.commit()
+        c.close()
+
         self.generic_visit(node)
 
-def main():
-    filename="test.py"
+def process_file(filename, conn):
     f = open(filename)
 
     a = ast.parse(f.read(), filename=filename)
 
-    visitor = Visitor(filename)
+    f.close()
+
+    visitor = Visitor(filename, conn)
 
     visitor.visit(a)
 
-    # map func calls to functions
+def find_inner_func(conn, filename, start_line, end_line):
+    # results may be greater than one if multiple files have the same name
 
-    f.close()
+    c = conn.cursor()
+    rows = c.execute("SELECT * FROM functions where filename = (%s) and start_line <= (%s) and end_line >= (%s)",
+            (filename, start_line, end_line, ))
+    keys = [k[0].decode('ascii') for k in c.description]
+    results = [dict(zip(keys, row)) for row in rows]
+    c.close()
+
+    return results
+
+def find_func(conn, base_name, func_name):
+    filename = base_name + ".py"
+
+    c = conn.cursor()
+    rows = c.execute("SELECT * FROM functions where filename = (%s) and name = (%s)",
+            (filename, func_name, ))
+    keys = [k[0].decode('ascii') for k in c.description]
+    results = [dict(zip(keys, row)) for row in rows]
+    c.close()
+
+    return results
+
+def handle_related_funcs(conn):
+    # map func calls to functions
+    c = conn.cursor()
+    rows = c.execute("SELECT * FROM func_call",)
+    keys = [k[0].decode('ascii') for k in c.description]
+    results = [dict(zip(keys, row)) for row in rows]
+    c.close()
+
+    for res in results:
+        # Threat to validity dont properly check which file the functions are apart of if both files have the same name and func name
+        # Threat to validity dont handle functions that are part of a class differenty from a class
+
+        # TODO also check if in class
+
+        # find func it is in
+        callers = find_inner_func(conn, res["filename"], res["start_line"], res["end_line"])
+
+        # find func location
+        funcs = find_func(conn, res["base_name"], res["name"])
+
+        # if funcs and callers exist match in related_funcs
+        if len(callers) > 0 and len(funcs) > 0:
+            # combine
+            for caller in callers:
+                for func in funcs:
+                    c = conn.cursor()
+                    c.execute("INSERT INTO related_funcs (caller_id, called_id) VALUES (%s, %s)",
+                            (caller["id"], func["id"], ))
+                    conn.commit()
+                    c.close()
+
+
+def main():
+    # connect to db
+    conn = pg8000.connect(user="postgres", password="pass", database="review_recomender")
+
+    # parse python file
+    filename="test.py"
+    process_file(filename, conn)
+
+    handle_related_funcs(conn)
+
+    conn.close()
+
 
 main()
